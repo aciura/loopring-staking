3cacb15ee118f24f5449a5f2858add45511faf50
Adrian Ciura <aciura@gmail.com>
2020-04-05 22:05:52 +0200

    Adding Ganashe local address

--- staking-ui/src/LrcContract.js
+++ staking-ui/src/LrcContract.js
@@ -1,19 +1,20 @@
-import Web3 from "web3";
-import LrcContract from "./contracts/LRC_v2.json";
-import UserStakingPool from "./contracts/UserStakingPool.json";
+import Web3 from 'web3'
+import LrcContract from './contracts/LRC_v2.json'
+import UserStakingPool from './contracts/UserStakingPool.json'
 
-const truffleNode = "http://127.0.0.1:9545/";
+// const truffleNode = "http://127.0.0.1:9545/"; //Truffle develop
+const truffleNode = 'http://127.0.0.1:7545' //GANASHE node
 
-export const web3 = new Web3(truffleNode);
+export const web3 = new Web3(truffleNode)
 
-const lrcNetworkDeploymentKey = Object.keys(LrcContract.networks)[0];
+const lrcNetworkDeploymentKey = Object.keys(LrcContract.networks)[0]
 export const loopringContract = new web3.eth.Contract(
   LrcContract.abi,
-  LrcContract.networks[lrcNetworkDeploymentKey].address
-);
+  LrcContract.networks[lrcNetworkDeploymentKey].address,
+)
 
-const uspNetworkDeploymentKey = Object.keys(UserStakingPool.networks)[0];
+const uspNetworkDeploymentKey = Object.keys(UserStakingPool.networks)[0]
 export const userStakingPoolContract = new web3.eth.Contract(
   UserStakingPool.abi,
-  UserStakingPool.networks[uspNetworkDeploymentKey].address
-);
+  UserStakingPool.networks[uspNetworkDeploymentKey].address,
+)
