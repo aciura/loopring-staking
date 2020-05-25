07c2cd7cacafa533ff1f7734d6edc89d3d36e11f
Adrian Ciura <aciura@gmail.com>
2020-05-25 10:30:01 +0200

    Added build folder to deploy

---
+++ build/precache-manifest.f84fab8fa7d8243f62d3b7b113d8434b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d006fa74940b420b686ea7268f77d91d",
+    "url": "/loopring-staking/index.html"
+  },
+  {
+    "revision": "5c3ca82b4f172b777ee6",
+    "url": "/loopring-staking/static/css/main.be772805.chunk.css"
+  },
+  {
+    "revision": "5b1eea2911c35eba7584",
+    "url": "/loopring-staking/static/js/2.445e49ed.chunk.js"
+  },
+  {
+    "revision": "deea066462e3852fb7f610c3fa67aa20",
+    "url": "/loopring-staking/static/js/2.445e49ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c3ca82b4f172b777ee6",
+    "url": "/loopring-staking/static/js/main.4777b7a0.chunk.js"
+  },
+  {
+    "revision": "a3f9c9b514495a21d1d8",
+    "url": "/loopring-staking/static/js/runtime-main.0410f88d.js"
+  }
+]);
