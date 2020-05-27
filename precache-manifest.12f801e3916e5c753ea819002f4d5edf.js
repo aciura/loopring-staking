11e41c6709fe9cc2c470b55479224401accbc5ff
Adrian Ciura <aciura@gmail.com>
2020-05-27 18:10:48 +0200

    Updates

---
+++ precache-manifest.12f801e3916e5c753ea819002f4d5edf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbf03941caafe8b4c24d1c9b9ea3a027",
+    "url": "/loopring-staking/index.html"
+  },
+  {
+    "revision": "013287cf01559789f318",
+    "url": "/loopring-staking/static/css/main.7101537a.chunk.css"
+  },
+  {
+    "revision": "ba0535fc49d813e5c4da",
+    "url": "/loopring-staking/static/js/2.4ac96e39.chunk.js"
+  },
+  {
+    "revision": "deea066462e3852fb7f610c3fa67aa20",
+    "url": "/loopring-staking/static/js/2.4ac96e39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "013287cf01559789f318",
+    "url": "/loopring-staking/static/js/main.8f950705.chunk.js"
+  },
+  {
+    "revision": "a3f9c9b514495a21d1d8",
+    "url": "/loopring-staking/static/js/runtime-main.0410f88d.js"
+  }
+]);
